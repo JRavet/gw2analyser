b99e0c4d1468127889ffcd03db2fc589917c00d4
JRavet <ravetjs@plu.edu>
2017-07-03 14:53:39 -0700

    Removed v2 DB user from config

--- tinymvc/myapp/configs/config_database.php
+++ tinymvc/myapp/configs/config_database.php
@@ -13,7 +13,7 @@ $config['default']['plugin'] = 'TinyMVC_PDO'; // plugin for db access
 $config['default']['type'] = 'mysql';      // connection type
 $config['default']['host'] = 'localhost';  // db hostname
 $config['default']['name'] = 'Gw2Analyserv2';     // db name
-$config['default']['user'] = 'gw2datacollectorv2';     // db username
+$config['default']['user'] = 'gw2datacollector';     // db username
 $config['default']['pass'] = 'egamirrorimeht';     // db password
 $config['default']['persistent'] = false;  // db connection persistence?
 
