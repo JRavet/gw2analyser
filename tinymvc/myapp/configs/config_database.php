7f3a6267cef099aa24bbbbe2ac76489d5fd700b3
JRavet <ravetjs@plu.edu>
2017-07-06 19:54:28 -0700

    Renamed schema file, and updated DB config accordingly

--- tinymvc/myapp/configs/config_database.php
+++ tinymvc/myapp/configs/config_database.php
@@ -12,7 +12,7 @@
 $config['default']['plugin'] = 'TinyMVC_PDO'; // plugin for db access
 $config['default']['type'] = 'mysql';      // connection type
 $config['default']['host'] = 'localhost';  // db hostname
-$config['default']['name'] = 'Gw2Analyserv2';     // db name
+$config['default']['name'] = 'Gw2Analyser';     // db name
 $config['default']['user'] = 'gw2datacollector';     // db username
 $config['default']['pass'] = 'egamirrorimeht';     // db password
 $config['default']['persistent'] = false;  // db connection persistence?
