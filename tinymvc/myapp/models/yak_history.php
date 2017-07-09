c18dd03424a1eab70c1b54efd5a464a4a3d6a60c
JRavet <ravetjs@plu.edu>
2017-07-08 23:57:33 -0700

    Added new model for new table : yak_history

---
+++ tinymvc/myapp/models/yak_history.php
@@ -0,0 +1,16 @@
+<?php
+/*
+id int(11) unsigned AUTO_INCREMENT,
+timeStamp datetime,
+capture_history_id int(11) unsigned,
+num_yaks int(4),
+PRIMARY KEY (id),
+FOREIGN KEY(capture_history_id) REFERENCES capture_history(id)
+ON DELETE CASCADE
+*/
+class yak_history extends TinyMVC_Model
+{
+
+}
+
+?>
