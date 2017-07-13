d8e1dbc58cd0e61d7d207efbb4c6548aeec3d1e5
JRavet <ravetjs@plu.edu>
2017-07-12 20:35:00 -0700

    Modified logic to use only a single match-data/id to function. Finished synchronize method (pending testing)

--- tinymvc/myapp/controllers/active_collector.php
+++ tinymvc/myapp/controllers/active_collector.php
@@ -13,9 +13,9 @@ if (class_exists('Active_Collector_Controller', false) === false)
 	{
 		private $api;
 
-		public function __construct($region)
+		public function __construct($match_id)
 		{
-			$this->api = new gw2_api($region);
+			$this->api = new gw2_api($match_id);
 			$this->main_loop(); // start the collector
 		}
 
@@ -33,28 +33,27 @@ if (class_exists('Active_Collector_Controller', false) === false)
 				$begin_time = microtime(true); //get the current time in microseconds; used to calculate processing time
 				$timeStamp = Date("Y-m-d H:i:s"); //make a unique timestamp to pass to functions that store data with timestamps
 
-				foreach( $this->api->get_matches() as $match )
+				$match = $api->get_match_data();
+
+				if ( $sync_data['new_week'] == TRUE )
 				{
-					if ( $sync_data['new_week'] == TRUE )
-					{
-						$this->store_match_details($match);
-					}
-					$this->store_capture_history($match, $tick_timer, $timeStamp);
-					$this->store_scores($match, $tick_timer, $timeStamp);
-				} // END foreach
+					$this->store_match_details($match);
+				}
+				$this->store_capture_history($match, $tick_timer, $timeStamp);
+				$this->store_scores($match, $tick_timer, $timeStamp);
 
 				if ( $sync_data['new_week'] === TRUE )
 				{ // if new match-details were stored, don't do it again
 					$sync_data['new_week'] = FALSE;
 				}
 
-				$diff = (microtime(true) - $begin_time)*SECONDS;
-				$idle_time = ((30*SECONDS) - $diff);
+				$processing_time = (microtime(true) - $begin_time)*SECONDS;
+				$idle_time = ((30*SECONDS) - $processing_time);
 
-				if ($tick_timer == 0.5 || $diff > (30*SECONDS))
+				if ( $tick_timer == 0.5 || $processing_time > (30*SECONDS) )
 				{
-					// TODO if diff > TIME, apply corrections but do not force resync
-					$sync_data = $this->synchronize($sync_data, $diff);
+					// TODO if processing_time > TIME, apply corrections but do not force resync
+					$sync_data = $this->synchronize($sync_data, $processing_time);
 					$idle_time = 1;
 					$tick_timer = 5.5;
 				}
@@ -68,53 +67,52 @@ if (class_exists('Active_Collector_Controller', false) === false)
 				}
 			} // end looping
 		}
-		private function synchronize($sync_data, $diff) //TODO rename $diff
+		private function synchronize($sync_data, $processing_time)
 		{
-			if ($diff >= (30*SECONDS))
+			if ($processing_time >= (30*SECONDS))
 			{ //if the processing time was over 30 seconds, no need to idle before syncing
 				$sync_data['sync_wait'] = FALSE; //just to ensure it doesn't wait extra time
 			}
 
-			if ( $sync_data['sync_wait'] === TRUE && $diff < (25*SECONDS) )
+			if ( $sync_data['sync_wait'] === TRUE && $processing_time < (25*SECONDS) )
 			{ //if there should be an initial delay, and the processing-time wasnt too long, idle for some time
-				usleep(23*SECONDS - $diff); //sleep for a combined (processing+idle) time of 25 seconds
+				usleep(23*SECONDS - $processing_time); //sleep for a combined (processing+idle) time of 25 seconds
 			}
 
-			$score_data = $this->api->get_matches();
-			//TODO set prev_score[$tiers] here
-			usleep(2*SECONDS); // wait 2 seconds so the score data just collected will be different
+			$prev_match = $this->api->get_scores();
+
+			usleep(2*SECONDS); // wait 2 seconds so the score data just collected will be processing_timeerent
 
 			while (TRUE)
 			{
-				$matches = $this->api->get_matches();
+				$current_match = $this->api->get_scores();
 
-				// TODO set current_score[$tiers] here
+				$current_score = $current_match->scores->red + $current_match->scores->blue + $current_match->scores->green;
+				$prev_score = $prev_match->scores->red + $prev_matches->scores->blue + $prev_matches->scores->green;
 
-				//$current_score = $matches[0]->scores->green + $matches[0]->scores->blue + $matches[0]->scores->red;
+				if ( $current_score >= ($prev_score + 200) )
+				{ // and a tick did occur
+					echo "sync complete\n";
+					break; // done syncing
+				}
 
-				// TODO foreach compare by [$tiers] here
-				// $current_score >= ($prev_score+230)
-				// array of boolean values by [$tier] if $tier_ready
-				if ($all_tiers_ready == TRUE) // TODO <-- is shorthand
-				{
-					$new_start_time = $matches[0]->start_time;
+				$prev_matches = $current_matches;
 
-					if ($new_start_time != $sync_data["prev_start_time"])
-					{
-						$new_week = TRUE;
-					}
+				usleep(1*SECONDS);
+			}
 
-					return array(
-						"new_week" => $new_week,
-						"prev_start_time" => $new_start_time,
-						"sync_wait" => TRUE
-					);
-				} // END if all_tiers_ready == TRUE
+			$new_start_time = $current_match->start_time;
 
-				$prev_score = $current_score;
+			if ($new_start_time != $sync_data["prev_start_time"])
+			{
+				$new_week = TRUE;
+			}
 
-				usleep(2*SECONDS);
-			} // END while
+			return array(
+				"new_week" => $new_week,
+				"prev_start_time" => $new_start_time,
+				"sync_wait" => TRUE // always do an extra sync-delay after the initial no-wait sync
+			);
 		}
 		private function store_scores()
 		{
@@ -171,9 +169,9 @@ if (class_exists('Active_Collector_Controller', false) === false)
 	$collector_started = true; // a hack to make this file load the framework AND execute itself
 } // END if not-class-exists
 if ($collector_started === true) { // a hack to make this file load the framework AND execute itself
-	if ( !in_array($argv[1], array(1,2)) )
+	if ( !isset($argv[1]) )
 	{
-		echo "Invalid region specified: " . $argv[1] . "\nExiting.\n";
+		echo "Invalid match specified: " . $argv[1] . "\nExiting.\n";
 		exit;
 	}
 	$tmvc->main('active_collector', null, $argv[1]); // start the active collector
