216b7bda9b09d5fafde14b999eb3ed79cfd4553e
JRavet <ravetjs@plu.edu>
2017-07-13 22:19:36 -0700

    Removed num_yaks_est from capture_history

--- tinymvc/myapp/models/capture_history.php
+++ tinymvc/myapp/models/capture_history.php
@@ -6,20 +6,19 @@ timeStamp datetime,
 last_flipped datetime,
 obj_id  varchar(10),
 owner_server int(4),
-tick_timer float(3,1),
 owner_color enum("Blue","Red","Green","Neutral"),
-num_yaks_est int(3),
+tick_timer float(3,1),
 num_yaks int(3),
 duration_owned time,
 PRIMARY KEY(id),
-FOREIGN KEY(obj_id) REFERENCES objective(obj_id)
-ON DELETE CASCADE,
+FOREIGN KEY(obj_id) REFERENCES objective(obj_id),
 FOREIGN KEY(match_detail_id) REFERENCES match_detail(id)
 ON DELETE CASCADE
 */
 class capture_history extends TinyMVC_Model
 {
-
+	protected $_table = "capture_history";
+	protected $pk = "id";
 }
 
 ?>
