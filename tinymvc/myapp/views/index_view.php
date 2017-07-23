f62d63516b03144dcb244567800e46fe47fa38d6
Claire <claire.lindblom@gmail.com>
2017-07-22 18:14:09 -0700

    Reformats code for table to remove echos

--- tinymvc/myapp/views/index_view.php
+++ tinymvc/myapp/views/index_view.php
@@ -9,38 +9,50 @@
 	</head>
 	<body>
 	<br/>
-		<?php
-			echo "<table class=' table-bordered'><tr>";
-			foreach ($data[0] as $key=>$value)
-			{
-				if (preg_match("/details|id/i",$key)) continue;
-				echo "<th>$key</th>";
-			}
-			echo "</tr>";
-			foreach($data as $ch)
-			{
-				if (preg_match("/Ruins|Spawn/i",$ch['place'])) {continue;}
-				echo "<tr style='Background-color: $ch[owner_color]'>";
-				foreach($ch as $key=>$value)
-				{
-					if (preg_match("/details|id/i",$key)) continue;
-					echo "<td>$value</td>";
-				}
-				echo "</tr>";
 
+			<table class='table table-bordered data-table'>
+				<tr>
+			<?php
+			foreach ($data[0] as $key=>$value) {
+				if (preg_match("/details|id/i",$key)) continue; ?>
+				<th><?=$key?></th>
+			<?php
+			}
+			?>
+				</tr>
+			<?php
+			foreach($data as $ch) {
+				if (preg_match("/Ruins|Spawn/i",$ch['place'])) continue; ?>
+
+				<tr style="Background-color: <?=$ch["owner_color"]?>">
+				<?php
+				foreach($ch as $key=>$value) {
+					if (preg_match("/details|id/i",$key)) continue; ?>
+					<td><?=$value?></td>
+				<?php
+				}
+				?>
+				</tr>
+				<?php
 				foreach($ch['details'] as $c)
 				{
-					echo "<tr><td>".$c['type']."</td>";
+				?>
+					<tr>
+						<td><?= $c['type'] ?></td>
+					<?php
 					foreach($c as $k=>$v)
 					{
-						if (preg_match("/type|capture_history_id|^id/i",$k)) {continue;}
-						echo "<td>$k=>$v</td>";
+						if (preg_match("/type|capture_history_id|^id/i",$k)) continue; ?>
+						<td><?=$k .'=>'. $v?></td>
+					<?php
 					}
-					echo "</tr>";
+					?>
+					</tr>
+					<?php
 				}
 			}
-			echo "</table>";
-		?>
+			?>
+			</table>
 	</body>
 	<script>
 	<?=$js?>
