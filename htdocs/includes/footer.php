1f309a06b2834d7310c9c1cd2c3da26a0dad5bca
JRavet <ravetjs@plu.edu>
2017-07-23 16:54:23 -0700

    Added header/footer includes for views

---
+++ htdocs/includes/footer.php
@@ -0,0 +1,14 @@
+</body>
+
+<script src="../js/jquery.min.js"></script> 
+<script src="../js/jquery.ui.custom.js"></script> 
+<script src="../js/bootstrap.min.js"></script> 
+<script src="../js/bootstrap-colorpicker.js"></script> 
+<script src="../js/bootstrap-datepicker.js"></script> 
+<script src="../js/jquery.toggle.buttons.js"></script>
+<script src="../js/jquery.uniform.js"></script> 
+<script src="../js/select2.min.js"></script> 
+<script src="../js/matrix.js"></script> 
+<script src="../js/matrix.form_common.js"></script>
+
+</html>
