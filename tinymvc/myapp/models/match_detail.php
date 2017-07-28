091a1ac764b3d0088cc586b6cc26021917703913
JRavet <ravetjs@plu.edu>
2017-07-28 16:36:55 -0700

    Added function to match_detail model which retrieves an assortment of related details for all matchups

--- tinymvc/myapp/models/match_detail.php
+++ tinymvc/myapp/models/match_detail.php
@@ -12,6 +12,32 @@ class match_detail extends TinyMVC_Model
 	protected $_table = "match_detail";
 	protected $pk = "id";
 
+	public function find_readable()
+	{
+		$data = $this->db->pdo->query("
+			SELECT md.id, md.start_time, md.end_time,
+				(SELECT GROUP_CONCAT(name SEPARATOR ', ') as red_servers
+					FROM server_linking sl
+					LEFT JOIN server_info si on si.server_id = sl.server_id
+					WHERE server_color = 'Red' AND md.id = sl.match_detail_id
+				) red_servers,
+				(SELECT GROUP_CONCAT(name SEPARATOR ', ') as blue_servers
+					FROM server_linking sl
+					LEFT JOIN server_info si on si.server_id = sl.server_id
+					WHERE server_color = 'Blue' AND md.id = sl.match_detail_id
+				) blue_servers,
+				(SELECT GROUP_CONCAT(name SEPARATOR ', ') as green_servers
+					FROM server_linking sl
+					LEFT JOIN server_info si on si.server_id = sl.server_id
+					WHERE server_color = 'Green' AND md.id = sl.match_detail_id
+				) green_servers
+			FROM match_detail md
+			ORDER BY md.start_time DESC;"
+		);
+
+		return $data;
+
+	}
 }
 
 ?>
