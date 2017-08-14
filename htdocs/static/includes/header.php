c0cda5b36e7985175357c070b3f769c89f9949ec
Claire <claire.lindblom@gmail.com>
2017-08-13 17:40:41 -0700

    Adds a user drop down and sets a default user for dev purposes

--- htdocs/static/includes/header.php
+++ htdocs/static/includes/header.php
@@ -15,23 +15,32 @@
 	<body style="background-color: white">
 	<div id="user-nav" class="navbar">
 	  <ul class="nav">
-	        <li><a href="login.html">Log Out</a></li>
-		 	<li  class="dropdown" id="table-analyser-dropdown" ><a title="" href="#" data-toggle="dropdown" data-target="#table-analyser-dropdown" class="dropdown-toggle"><span class="text">Analyser Tables</span><b class="caret"></b></a>
+
+		 	<li class="dropdown" id="table-analyser-dropdown" ><a title="" href="#" data-toggle="dropdown" data-target="#table-analyser-dropdown" class="dropdown-toggle"><span class="text">Analyser Tables</span><b class="caret"></b></a>
+			  <ul class="dropdown-menu">
+				<li><a href="#">Capture History</a></li>
+				<li class="divider"></li>
+				<li><a href="#">Guild History</a></li>
+				<li class="divider"></li>
+				<li><a href="#">Match History</a></li>
+			  </ul>
+			</li>
+
+			<li class="dropdown" id="graphical-analyser-dropdown" ><a title="" href="#" data-toggle="dropdown" data-target="#graphical-analyser-dropdown" class="dropdown-toggle"><span class="text">Analyser Charts</span><b class="caret"></b></a>
+			  <ul class="dropdown-menu">
+				<li><a href="#">Score Graphs</a></li>
+				<li class="divider"></li>
+				<li><a href="#">Historical Map</a></li>
+			  </ul>
+			</li>
+
+			<li class="dropdown" id="user-profile" ><a title="" href="#" data-toggle="dropdown" data-target="#user-profile" class="dropdown-toggle"><span class="text"><?=$username?></span><b class="caret"></b></a>
 		      <ul class="dropdown-menu">
-		        <li><a href="#">Capture History</a></li>
+		        <li><a href="#"><i class="icon-user"></i>Preferences</a></li>
 		        <li class="divider"></li>
-		        <li><a href="#">Guild History</a></li>
-		        <li class="divider"></li>
-		        <li><a href="#">Match History</a></li>
+		        <li><a href="#"><i class="icon-key"></i> Log Out</a></li>
 		      </ul>
-		    </li>
-		     <li  class="dropdown" id="graphical-analyser-dropdown" ><a title="" href="#" data-toggle="dropdown" data-target="#graphical-analyser-dropdown" class="dropdown-toggle"><span class="text">Analyser Charts</span><b class="caret"></b></a>
-		      <ul class="dropdown-menu">
-		        <li><a href="#">Score Graphs</a></li>
-		        <li class="divider"></li>
-		        <li><a href="#">Historical Map</a></li>
-		      </ul>
-		    </li>
+    		</li>
 	  </ul>
 	</div>
 	</br>
