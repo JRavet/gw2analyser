b98f7f3538f9e87b2b44c4fadb7ca5c2a253ead1
JRavet <ravetjs@plu.edu>
2017-08-21 21:31:39 -0700

    Moved most files out of htdocs and into root directory

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if(!defined('DS'))
 
 /* set the base directory */
 if(!defined('TMVC_BASEDIR'))
-  define('TMVC_BASEDIR',dirname(__FILE__) . DS . '..' . DS . 'tinymvc' . DS);
+  define('TMVC_BASEDIR',dirname(__FILE__) . DS . 'tinymvc' . DS);
 
 /* include main tmvc class */
 require(TMVC_BASEDIR . 'sysfiles' . DS . 'TinyMVC.php');
