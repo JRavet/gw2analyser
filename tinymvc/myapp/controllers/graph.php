b2d23f8816493d51825dee3bd03505b29f09af52
JRavet <ravetjs@plu.edu>
2017-11-28 20:24:14 -0800

    Fixed typo

--- tinymvc/myapp/controllers/graph.php
+++ tinymvc/myapp/controllers/graph.php
@@ -43,7 +43,7 @@ class Graph_Controller extends TinyMVC_Controller
 					// the second parameter in above is required because skirmish_score doesn't have maps
 				$skirmish_history = $this->skirmish_score->getScores($params);
 
-				$this->view->assign("score_history", $score_history);
+				$this->view->assign("scores", $score_history);
 				$this->view->assign("skirmish_points", $skirmish_history);
 			} else {
 				$this->view->assign("error", $error);
